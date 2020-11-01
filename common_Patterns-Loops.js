3d7e4060edd115028a54b90705f0523741eb62e8
Vasilios Kambouras <Vasilioskw@gmail.com>
2020-11-01 14:47:52 -0500

    forgot to commit this

---
+++ common_Patterns-Loops.js
@@ -0,0 +1,41 @@
+// Arrays
+
+// const todos = ["pet the cat", "go to work", "shop for groceries", "go home", "feed the cat"]
+
+// const firstItem = todos[0]
+// console.log(firstItem);
+
+// const secondItem = todos[1]
+// console.log(secondItem);
+
+// (simpifid)
+
+
+
+const todos = ["pet the cat", "go to work", "shop for groceries", "go home", "feed the cat"]
+
+console.log(todos[0]);
+// "pet the cat"
+
+console.log(todos[1]);
+// "go to work"
+
+
+
+
+
+// Accumulating values
+
+
+function lets_Count_To_Ten(numbers){
+
+    let n = 1;
+    while (n <10) 
+      console.log(n);
+      n++;
+      if (n === 10) break;
+      
+
+}
+
+lets_Count_To_Ten(numbers)
